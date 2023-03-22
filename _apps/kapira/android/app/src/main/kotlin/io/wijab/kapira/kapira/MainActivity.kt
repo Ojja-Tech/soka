b8245d29a3479916d693c9b03a5479dc872c2a56
Isaac Obella <wizlif@users.noreply.github.com>
2023-03-23 00:05:21 +0300

    init: Base apps
    Signed-off-by: Isaac Obella <wizlif@users.noreply.github.com>

---
+++ _apps/kapira/android/app/src/main/kotlin/io/wijab/kapira/kapira/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.wijab.kapira.kapira
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ _apps/kapira_admin/android/app/src/main/kotlin/io/wijab/kapira_admin/kapira_admin/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.wijab.kapira_admin.kapira_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
