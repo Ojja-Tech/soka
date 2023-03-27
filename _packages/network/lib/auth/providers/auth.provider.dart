b432776b73d32531eff694519adb6fe823691700
Isaac Obella <wizlif@users.noreply.github.com>
2023-03-27 10:55:15 +0300

    fix: Exception handling
    Signed-off-by: Isaac Obella <wizlif@users.noreply.github.com>

--- _packages/network/lib/auth/providers/auth.provider.dart
+++ _packages/network/lib/auth/providers/auth.provider.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:network/auth/repository/auth_repository_impl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -6,6 +7,26 @@ part 'auth.provider.g.dart';
 
 @Riverpod(keepAlive: true)
 Future<AuthRepositoryImpl> authProvider(AuthProviderRef ref) async {
-  FirebaseAuth auth = FirebaseAuth.instance;
-  return AuthRepositoryImpl(auth: auth);
+  final firebaseAuth = ref.read(firebaseAuthProvider);
+  final googleSignIn = ref.read(googleSignInProvider);
+  
+  return AuthRepositoryImpl(
+    firebaseAuth: firebaseAuth,
+    googleSignIn: googleSignIn,
+  );
+}
+
+@Riverpod(keepAlive: true)
+GoogleSignIn googleSignIn(GoogleSignInRef ref) {
+  return GoogleSignIn(
+    scopes: [
+      'email',
+      'https://www.googleapis.com/auth/contacts.readonly',
+    ],
+  );
+}
+
+@Riverpod(keepAlive: true)
+FirebaseAuth firebaseAuth(FirebaseAuthRef ref) {
+  return FirebaseAuth.instance;
 }
